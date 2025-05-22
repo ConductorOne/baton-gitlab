267550575cf95511ae3409ee0d4dfa10222247f4
Mateo Vespasiano <mateo.vespasiano@conductorone.com>
2025-05-22 09:00:04 -0300

    test setup to support both versions

--- pkg/connector/gitlab/client.go
+++ pkg/connector/gitlab/client.go
@@ -2,6 +2,8 @@ package gitlab
 
 import (
 	"context"
+	"net/http"
+	"strconv"
 
 	"github.com/conductorone/baton-sdk/pkg/uhttp"
 	gitlabSDK "gitlab.com/gitlab-org/api/client-go"
@@ -10,6 +12,7 @@ import (
 type Client struct {
 	*gitlabSDK.Client
 	AccountCreationGroup string
+	IsOnPremise          bool
 }
 
 func NewClient(ctx context.Context, accessToken, baseURL, accountCreationGroup string) (*Client, error) {
@@ -29,5 +32,40 @@ func NewClient(ctx context.Context, accessToken, baseURL, accountCreationGroup s
 	return &Client{
 		Client:               client,
 		AccountCreationGroup: accountCreationGroup,
+		IsOnPremise:          baseURL != "https://gitlab.com/",
 	}, nil
+}
+
+func (c *Client) GetAllUsers(ctx context.Context, nextPageToken string) ([]gitlabSDK.User, *gitlabSDK.Response, error) {
+	var nextPage int
+	var err error
+
+	if nextPageToken != "" {
+		nextPage, err = strconv.Atoi(nextPageToken)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	usersPath := "users"
+	opt := &gitlabSDK.ListUsersOptions{
+		ListOptions: gitlabSDK.ListOptions{
+			Page: nextPage,
+		},
+	}
+	var options []gitlabSDK.RequestOptionFunc
+	options = append(options, gitlabSDK.WithContext(ctx))
+
+	req, err := c.NewRequest(http.MethodGet, usersPath, opt, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var users []gitlabSDK.User
+	resp, err := c.Do(req, &users)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return users, resp, nil
 }
