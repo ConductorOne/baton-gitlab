4c9aa64e2ab69728a0a4e30efeb8cd22e3ff9853
Luisina Santos <luisina.santos@conductorone.com>
2025-07-18 13:05:24 -0300

    add support to paginate invited users and list only owned groups and projects

--- pkg/connector/gitlab/groups_endpoints.go
+++ pkg/connector/gitlab/groups_endpoints.go
@@ -25,6 +25,7 @@ func (o *Client) ListGroups(ctx context.Context, nextPageStr string) ([]*gitlabS
 		ListOptions: gitlabSDK.ListOptions{
 			Page: nextPage,
 		},
+		Owned: gitlabSDK.Ptr(true),
 	},
 		gitlabSDK.WithContext(ctx),
 	)
@@ -150,4 +151,40 @@ func (o *Client) RemoveGroupMember(ctx context.Context, groupId string, userId i
 	}
 
 	return nil
+}
+
+func (o *Client) ListInvitedUsersPaginate(ctx context.Context, groupId string, nextPageStr string) ([]*gitlabSDK.GroupMember, *gitlabSDK.Response, error) {
+	if nextPageStr == "" {
+		return nil, nil, fmt.Errorf("gitlab-connector: no page given for pagination")
+	}
+
+	var nextPage int
+	var err error
+
+	if nextPageStr != "" {
+		nextPage, err = strconv.Atoi(nextPageStr)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	if nextPage < 1 {
+		return nil, nil, fmt.Errorf("gitlab-connector: invalid page given for pagination: %d", nextPage)
+	}
+	users, res, err := o.Groups.ListAllGroupMembers(groupId, &gitlabSDK.ListGroupMembersOptions{
+		ListOptions: gitlabSDK.ListOptions{
+			Page:    nextPage,
+		},
+	},
+		gitlabSDK.WithContext(ctx),
+	)
+	if err != nil {
+		return nil, res, err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return nil, res, err
+	}
+
+	return users, res, nil
 }
